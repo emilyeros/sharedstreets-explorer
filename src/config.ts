535eb270d2f5a9cb46f5602cf3d7e5725e3e682e
Kevin Webb <kpwebb@gmail.com>
2018-08-14 12:35:03 -0400

    changed "events" label to default

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,6 @@ export const SS_TILE_URL = 'https://tiles.sharedstreets.io/planet-180430';
 export const PICKUP_EVENT_LABEL = 'pickup';
 export const DROPOFF_EVENT_LABEL = 'dropoff';
 
-export const EVENT_FILE_TYPE = 'curb';
+export const EVENT_FILE_TYPE = 'events';
 
 export const DOT_SIZE_MAX_VALUE = 100;
